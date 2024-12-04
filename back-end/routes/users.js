75ea4c74f03dcdf866532ba342fde41de2893353
ngocanh0701 <ntngocanh701@gmail.com>
2024-12-04 08:49:25 +0700

    submit

--- back-end/routes/users.js
+++ back-end/routes/users.js
@@ -23,16 +23,16 @@ const router = express.Router();
 // })
 
 //UPDATE
-router.put("/:id", verifyUser, updateUser);
+router.put("/:id", updateUser);
 
 //DELETE
-router.delete("/:id", verifyUser, deleteUser);
+router.delete("/:id", deleteUser);
 
 //GET
-router.get("/:id", verifyUser, getUser);
+router.get("/:id", getUser);
 
 //GET ALL
-router.get("/", verifyAdmin, getUsers);
+router.get("/", getUsers);
 
 router.get("/getuser", getUserbyNP);
 export default router;
